4e22838ad3f6fbec99538016b0f68b5d2c975a66
Brian Stout <brianjonstout@gmail.com>
2016-10-20 14:28:46 -0400

    Make insulter.c which contains a lot of string functions from the string.h library.

--- day3/insulter.c
+++ day3/insulter.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 
 int main(void)
 {
@@ -6,7 +9,31 @@ int main(void)
 
     printf("Please enter your name:  "); 
 
+    //TODO: Check results of gets
     fgets(name, sizeof(name), stdin);
 
-    printf("%s\n", name);
+    size_t len = strlen(name);
+
+    name[len -1] = '\0';
+
+    printf("%s is smelly\n", name);
+
+    if(0 == strncmp(name, "Stephen", sizeof(name))){
+        puts("Learn to phucking spell");
+    }
+    
+    strncat(name, "head", sizeof(name) - len);
+    printf("%s, %s is a stupid %s\n", name, name, name);
+
+    char buf[128];
+
+    //TODO: Check return value
+    snprintf(buf, sizeof(name), "PFC %s", name );
+    strncpy(name, buf, sizeof(name));
+
+    
+    printf("Ha, ha, %s is so dumb\n", name);
+
+
 }
+
