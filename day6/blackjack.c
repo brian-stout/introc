1c49356505374b8bdde7c176d2746dc1bb24a42a
Brian Stout <brianjonstout@gmail.com>
2016-10-25 15:51:33 -0400

    Add a deck_deal function, it returns how many cards are left in the deck, need to add a hand structure (literally a clone of hand) Pointers would make this easier, I have no idea how to modify the amount of cards in a hand and modify the amount of cards left in a deck (though that works really well right now))  I'm sure Liam will show up with a silly much more simple answer.

--- day6/blackjack.c
+++ day6/blackjack.c
@@ -1,5 +1,6 @@
 
 #include <stdio.h>
+#include <string.h>
 
 #include "card.h"
 #include "deck.h"
@@ -9,9 +10,25 @@ int main(void)
 	struct deck pile = deck_create();
 	char buf[18];
 
-	for(int n = 0; n < 52; ++n) {
+
+
+
+	for(int n = 0; n < pile.card_amt; ++n) {
 		card_format(buf, sizeof(buf), pile.cards[n]);
 
 		puts(buf);
 	}
+	printf("Deck pile has %d cards in it.\n", pile.card_amt);
+	
+
+	struct card hand[258];
+	pile.card_amt = deck_deal(pile, hand, 6);
+	card_format(buf, sizeof(buf), hand[0]);
+	for(int i = 0; i < 6; ++i){
+		card_format(buf, sizeof(buf), hand[i]);
+		puts(buf);
+	}
+
+	printf("Deck pile has %d cards in it.\n", pile.card_amt);
+
 }
--- day6/deck.c
+++ day6/deck.c
@@ -1,15 +1,16 @@
 
 #include "deck.h"
-
 #include <string.h>
+#include <stdio.h>
 
 struct deck deck_create(void)
 {
 	struct deck d;
+	d.card_amt = 0;
 	for(int s = 0; s < 4; ++s) {
 		for(int rank = 2; rank < 15; ++rank) {
 			size_t i = s*13 + (rank - 2);
-
+			++d.card_amt;
 			switch(s) {
 				case 0:
 					strncpy(d.cards[i].suit, "Clubs", sizeof(d.cards[i].suit));
@@ -24,10 +25,27 @@ struct deck deck_create(void)
 					strncpy(d.cards[i].suit, "Spades", sizeof(d.cards[i].suit));
 					break;
 			}
-
+			
 			d.cards[i].rank = rank;
 		}
 	}
 
 	return d;
 }
+
+int deck_deal(struct deck d, struct card hand[], int amount){
+	for(int i = 0; i < amount; ++i){
+		if (d.card_amt > 0){
+			hand[i].rank = d.cards[d.card_amt-1].rank;
+			strncpy(hand[i].suit, d.cards[i].suit, sizeof(d.cards[i].suit));
+			--d.card_amt;
+			printf("Deck has %d cards.\n", d.card_amt);
+		}
+		else{
+			printf("Deck is empty\n");
+		}
+	}
+	return d.card_amt;
+}
+
+
